6e8c4c99710744dee00d67f6a9749778e59bf8dc
devin de <122511845+davehe123@users.noreply.github.com>
2026-03-19 15:08:05 +0800

    Remove function

--- functions/api/remove-bg.js
+++
@@ -1,9 +0,0 @@
-export function onRequest(request) {
-  return new Response(JSON.stringify({ 
-    method: request.method,
-    url: request.url,
-    headers: Object.fromEntries(request.headers)
-  }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
